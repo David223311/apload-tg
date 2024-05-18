91161bea513cf88d0e79f8b3d74dced860f5a988
David223311 <112881596+David223311@users.noreply.github.com>
2024-05-18 18:42:53 +0300

    Вынес айди из функции

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -2,8 +2,8 @@ import requests
 from download_images import download_image
 
 
-def fetch_spacex_last_launch():
-    url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+def fetch_spacex_last_launch(launch_id):
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     spacex_links = response.json()['links']['flickr']['original']
@@ -13,7 +13,8 @@ def fetch_spacex_last_launch():
 
 
 def main():
-    fetch_spacex_last_launch()
+    launch_id = "5eb87d47ffd86e000604b38a"
+    fetch_spacex_last_launch(launch_id)
 
 
 if __name__ == '__main__':
